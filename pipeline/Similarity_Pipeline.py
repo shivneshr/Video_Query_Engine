c4b51b82b8fd9605bc9ba509f4d07c0a12981ed2
Shivnesh Rajan <shivnesh@Shivneshs-MacBook-Pro.local>
2018-04-28 20:31:55 -0700

    Modified the keyframe extraction to extract one frame per second, changed the similarity checker to compare query video with every database video and give results

--- pipeline/Similarity_Pipeline.py
+++ pipeline/Similarity_Pipeline.py
@@ -20,8 +20,7 @@ vgg_model = vgg16.VGG16(weights='imagenet')
 layer_name = 'fc2'
 
 # Specifying the output variable
-intermediate_layer_model = Model(inputs=vgg_model.input,
-								 outputs=vgg_model.get_layer(layer_name).output)
+intermediate_layer_model = Model(inputs=vgg_model.input, outputs=vgg_model.get_layer(layer_name).output)
 
 # Load the Inception_V3 model
 # inception_model = inception_v3.InceptionV3(weights='imagenet')
@@ -37,21 +36,29 @@ previousFolder = ""
 
 featureList = []
 
-# only if you want to keyframe extraction for specific folders
-onlyfor = ['first','second']
+# only if you want to CNN extraction for specific folders
+onlyfor = []
+
+
+keyframePath_database = "/Users/shivnesh/Documents/shivnesh_git/multimedia_project/keyframes_database/"
+keyframePath_query = "/Users/shivnesh/Documents/shivnesh_git/multimedia_project/keyframes_query/"
+queryPath = "/Users/shivnesh/Documents/shivnesh_git/multimedia_project/videos/query/"
+databasePath = "/Users/shivnesh/Documents/shivnesh_git/multimedia_project/videos/search/"
 
 
 
-p_frame_thresh = 98000  # You may need to adjust this threshold for keyframes extraction (lower -> more frames)
+p_frame_thresh = 90000  # You may need to adjust this threshold for keyframes extraction (lower -> more frames)
 
 
-def extractKeyFrames(video_path,videoName):
+def extractKeyFrames(video_path,videoName,keyframePath):
 	global p_frame_thresh
 
 	cap = cv2.VideoCapture(video_path,0)
 	# Read the first frame.
 	ret, prev_frame = cap.read()
 
+	if not os.path.exists(keyframePath + videoName):
+		os.makedirs(keyframePath + videoName)
 
 	count = 0
 	while ret:
@@ -64,26 +71,49 @@ def extractKeyFrames(video_path,videoName):
 			non_zero_count = np.count_nonzero(diff)
 			if non_zero_count > p_frame_thresh:
 				count += 1
-				if not os.path.exists("keyframes/"+videoName):
-					os.makedirs("keyframes/"+videoName)
-				cv2.imwrite("keyframes/%s/%s_frame%d.jpg" % (videoName,videoName,count), curr_frame)
+				cv2.imwrite(keyframePath+"%s/%s_frame%d.jpg" % (videoName,videoName,count), curr_frame)
 				print(count, "Got P-Frame")
 			prev_frame = curr_frame
 			prev_gray_image = cv2.cvtColor(prev_frame, cv2.COLOR_RGB2GRAY)
+	cv2.imwrite(keyframePath + "%s/%s_frame%d.jpg" % (videoName, videoName, count), prev_frame)
 
 
-def iterateThroughDirectory(directory):
+def extractKeyFrames_Second(video_path,videoName,keyframePath):
+
+	cap = cv2.VideoCapture(video_path)
+	# Read the first frame.
+	ret, frame = cap.read()
+
+	multiplier = 30
+
+	if not os.path.exists(keyframePath + videoName):
+		os.makedirs(keyframePath + videoName)
+
+	count = 0
+	while ret:
+		frameId = int(round(cap.get(1)))
+		ret, frame = cap.read()
+
+		if frameId % multiplier == 0 and ret:
+			count+=1
+			cv2.imwrite(keyframePath + "%s/%s_frame%d.jpg" % (videoName, videoName, count), frame)
+
+	cap.release()
+	print("Complete")
+
+
+def iterateThroughDirectory(directory,keyframepath):
 
 	for files in os.listdir(directory):
 		print("Processing ",files)
 		if os.path.isfile(directory + files):
-			extractKeyFrames(directory+files,files.replace(".mp4",""))
+			extractKeyFrames_Second(directory+files,files.replace(".mp4",""),keyframepath)
 
 
-def write_to_pickle_file(folder):
+def write_to_pickle_file(folder,keyframePath):
 	global featureList, currentFolder, previousFolder
 
-	location = 'keyframes/'+folder+'/'+folder+'.pickle'
+	location = keyframePath+folder+'/'+folder+'.pickle'
 	with open(location, 'wb') as handle:
 		pickle.dump(featureList, handle, protocol=pickle.HIGHEST_PROTOCOL)
 	currentFolder = previousFolder
@@ -100,21 +130,30 @@ def findDifference(queryPath,databasePath):
 	with open(queryPath+'/'+queryFileName+'.pickle', 'rb') as handle:
 		queryFeatures = pickle.load(handle)
 
-	with open(databasePath+'/'+databaseFileName+'.pickle', 'rb') as handle:
-		databaseFeatures = pickle.load(handle)
 
-	minDist = float('inf')
-
-	for feature_d in databaseFeatures:
-		for feature_q in queryFeatures:
-			diff = np.absolute(feature_d - feature_q)
-			sum = np.sum(diff)
-			print(sum)
-			if sum < minDist:
-				minDist = sum
+	match_factor = {}
 
 
-	print("match factor ",minDist)
+	for folder in os.listdir(databasePath):
+
+		with open(databasePath +  folder +"/" + folder + '.pickle', 'rb') as handle:
+			databaseFeatures = pickle.load(handle)
+
+		minDist = float('inf')
+
+		for feature_d in databaseFeatures:
+			for feature_q in queryFeatures:
+				diff = np.absolute(feature_d - feature_q)
+				sum = np.sum(diff)
+				if sum < minDist:
+					minDist = sum
+
+		match_factor[folder] = minDist
+
+
+
+	for key, value in sorted(match_factor.items(), key=lambda x: x[1]):
+		print("%s: %s" % (key, value))
 
 
 def extract_CNN_features(path):
@@ -124,7 +163,7 @@ def extract_CNN_features(path):
 	for root, dirs, files in os.walk(path):
 
 		if previousFolder != "" and currentFolder != previousFolder:
-			write_to_pickle_file(previousFolder)
+			write_to_pickle_file(previousFolder,path)
 
 		for name in files:
 
@@ -159,21 +198,28 @@ def extract_CNN_features(path):
 				featureList.append(intermediate_output[0])
 
 		if previousFolder != "" and currentFolder != previousFolder:
-			write_to_pickle_file(previousFolder)
+			write_to_pickle_file(previousFolder,path)
 
 
 
 # Extracts the keyframes of the set of videos and stores result in keyframes folder
-#iterateThroughDirectory("video/")
+#iterateThroughDirectory(databasePath,keyframePath_database)
 
 # Extracts the keyframes of the set of videos and stores result in keyframes folder
-#iterateThroughDirectory("query/")
+#iterateThroughDirectory(queryPath,keyframePath_query)
 
 # Extracts the CNN features for all the video files and stores in Pickle file in corresponding directory
-#extract_CNN_features("keyframes/")
 
-# Finds the similarity between any two given folders (lower the score better similarity)
-findDifference("keyframes/second","keyframes/musicvideo")
+	# For database videos
+#extract_CNN_features(keyframePath_database)
+
+	# For Query videos
+
+#extract_CNN_features(keyframePath_query)
+
+# Finds the similarity between any given query video & database folders (lower the score better similarity)
+
+findDifference(keyframePath_query+"Q5.MP4",keyframePath_database)
 
 
 
